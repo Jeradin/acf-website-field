91e80294e88a844000c6662e7222abf36f1b455f
Geet Jacobs <geet@anagr.am>
2013-04-11 13:54:17 -0600

    Updated add-on to work with ACF 4.0

--- website_url.php
+++ website_url.php
@@ -1,64 +1,289 @@
 <?php
 
 /*
- *	Advanced Custom Fields - New field template
- *
- *	Create your field's functionality below and use the function:
- *	register_field($class_name, $file_path) to include the field
- *	in the acf plugin.
- *
- *	Documentation:
- *
- */
-
+*  my-field.php
+*
+*  This file acts as a template for creating a new field type in the
+*  Advanced Custom Fields plugin.
+*
+*  @since	4.0.0
+*  @date	DD/MM/YYYY
+*
+*  @info	http://www.advancedcustomfields.com/docs/tutorials/creating-a-new-field-type/
+*/
 
 class Website_field extends acf_Field
 {
 
-	/*--------------------------------------------------------------------------------------
-	*
-	*	Constructor
-	*	- This function is called when the field class is initalized on each page.
-	*	- Here you can add filters / actions and setup any other functionality for your field
-	*
-	*	@author Elliot Condon
-	*	@since 2.2.0
-	*
-	*-------------------------------------------------------------------------------------*/
+	// vars
+	var  $defaults // will hold default field options
+		;
 
-	function __construct($parent)
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function __construct()
 	{
-		// do not delete!
-		parent::__construct($parent);
+		// vars
+		$this->name = 'website';
+		$this->label = __('Website');
+		$this->defaults = array(
+			'value'              => ''
+			,'internal_link'      => true
+			, 'website_title'      => true
 
-		// set name / title
-		$this->name = 'website'; // variable name (no spaces / special characters / etc)
-		$this->title = __("Website",'acf'); // field label (Displayed in edit screens)
+		);
+
+
+		// do not delete!
+    	parent::__construct();
+	}
+
+
+	/*
+	*  load_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded from
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in te database
+	*/
+
+	function load_value( $value, $post_id, $field )
+	{
+		return $value;
+	}
+
+
+
+
+
+	/*
+	*  format_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value( $value, $field )
+	{
+		return $value;
+	}
+
+
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+
+	function format_value_for_api(  $value, $post_id, $field )
+	{
+
+
+		// defaults
+		$field = array_merge($this->defaults, $field);
+		extract( $field, EXTR_SKIP ); //Declare each item in $field as its own variable i.e.: $name, $value, $label, $time_format, $date_format and $show_week_number
+/*
+		// format value
+		$value = explode('|', $value);
+
+		if( $field['val'] == 'address' )
+		{
+			$value = array( 'coordinates' => $value[1], 'address' => $value[0] );
+		}
+		else
+		{
+			$value = $value[1];
+		}
+
+
+		return $value;*/
+
+
+		// format value
+
+		$external = '';
+			//If an external link
+			if($internal_link=='true'  && !empty($value['internal']) ){ $external = 'target="_blank"';};
+
+			//If show title
+			if($website_title=='true' && !empty($value['title'])){ $title = $value['title'];}else{$title = ereg_replace("(https?)://", "", $value['url']);};
+
+				$value ='<a href="http://'.ereg_replace("(https?)://", "", $value['url']).'" '.$external.'>'.$title.'</a>';
+
+
+
+
+		// return value
+		    return $value;
+
+
+
+	}
+
+	/*
+	*  load_field()
+	*
+	*  This filter is appied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+
+	function load_field( $field )
+	{
+		return $field;
+	}
+
+
+	/*
+	*  update_field()
+	*
+	*  This filter is appied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field( $field, $post_id )
+	{
+			return $field;
+	}
+
+
+
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - an array holding all the field's data
+	*/
+
+	function create_field($field)
+	{
+
+			$field = array_merge( $this->defaults, $field );
+		extract( $field, EXTR_SKIP ); //Declare each item in $field as its own variable i.e.: $name, $value, $label, $time_format, $date_format and $show_week_number
+
+		echo  '<table class="widefat "><thead><tr>';
+
+		if ($website_title=='true')echo '<th class="title"><span>Title</span></th>';
+		echo '<th class="url"><span>URL</span></th>';
+		if ($internal_link=='true')echo '<th class="internal" style="width:10%;"><span>Internal Link</span></th>';
+
+		echo '</tr></thead><tbody><tr>';
+
+
+
+
+
+			if($website_title=='true'){
+
+				echo '<td><input type="text" value="' . $value['title'] . '" id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'].'[title]" /></td>';
+		}else{
+				echo '<input type="hidden" value="" id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'].'[title]" />';
+		}
+
+echo '<td><input type="text" value="' . $value['url'] . '" id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'].'[url]" /><p class="description">You can exclude http://, the field will add it, if missing.</p></td>';
+
+
+			if($internal_link=='true'){
+			echo '<td><ul class="checkbox_list true_false"><input type="hidden" name="'.$field['name'].'[internal]" value="0" />';
+				$selected = ($value['internal'] == 1) ? 'checked="yes"' : '';
+				echo '<li><label><input type="checkbox" name="'.$field['name'].'[internal]" value="1" ' . $selected . ' />';
+				echo '</label></li></ul></td>';
+
+			}else{
+				echo '<input type="hidden" name="'.$field['name'].'[internal]" value="0" />';
+			}
+
+
+		echo  '</tr>
+		</tbody>
+			</table>';
+
+
 
 	}
 
 
-	/*--------------------------------------------------------------------------------------
+	/*
+	*  create_options()
 	*
-	*	create_options
-	*	- this function is called from core/field_meta_box.php to create extra options
-	*	for your field
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
 	*
-	*	@params
-	*	- $key (int) - the $_POST obejct key required to save the options to the field
-	*	- $field (array) - the field object
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
 	*
-	*	@author Elliot Condon
-	*	@since 2.2.0
-	*
-	*-------------------------------------------------------------------------------------*/
+	*  @param	$field	- an array holding all the field's data
+	*/
 
-	function create_options($key, $field)
+	function create_options($field)
 	{
-		// defaults
-		$field['website_title'] = isset($field['website_title']) ? $field['website_title'] : '';
 
-		$field['internal_link'] = isset($field['internal_link']) ? $field['internal_link'] : '';
+
+
+		// defaults
+		$field = array_merge($this->defaults, $field);
+
+
+		// key is needed in the field names to correctly save the data
+		$key = $field['name'];
+
+		// defaults
+		//$field['website_title'] = isset($field['website_title']) ? $field['website_title'] : '';
+
+		//$field['internal_link'] = isset($field['internal_link']) ? $field['internal_link'] : '';
 
 ?>
 		<tr class="field_option field_option_<?php echo $this->name; ?>">
@@ -68,7 +293,8 @@ class Website_field extends acf_Field
 			</td>
 			<td>
 				<?php
-		$this->parent->create_field(array(
+
+		do_action('acf/create_field', array(
 				'type' => 'radio',
 				'name' => 'fields['.$key.'][website_title]',
 				'value' => $field['website_title'],
@@ -78,6 +304,8 @@ class Website_field extends acf_Field
 					'false' => __('No',acf)
 				)
 			));
+
+
 ?>
 			</td>
 
@@ -90,7 +318,7 @@ class Website_field extends acf_Field
 			</td>
 			<td>
 				<?php
-		$this->parent->create_field(array(
+		do_action('acf/create_field', array(
 				'type' => 'radio',
 				'name' => 'fields['.$key.'][internal_link]',
 				'value' => $field['internal_link'],
@@ -109,187 +337,91 @@ class Website_field extends acf_Field
 		<?php
 	}
 
-
-
-
-	/*--------------------------------------------------------------------------------------
+	/*
+	*  input_admin_enqueue_scripts()
 	*
-	*	create_field
-	*	- this function is called on edit screens to produce the html for this field
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add css + javascript to assist your create_field() action.
 	*
-	*	@author Elliot Condon
-	*	@since 2.2.0
-	*
-	*-------------------------------------------------------------------------------------*/
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
 
-	function create_field($field)
+	function input_admin_enqueue_scripts()
 	{
-
-		echo  '<table class="widefat "><thead><tr>';
-
-		if ($field['website_title'] == 'true')echo '<th class="title"><span>Title</span></th>';
-		echo '<th class="url"><span>URL</span></th>';
-		if ($field['internal_link'] == 'true')echo '<th class="internal" style="width:15.75%;"><span>Internal Link</span></th>';
-
-		echo '</tr></thead><tbody><tr>';
-
-
-		// defaults
-		if(empty($field['value'])) $field['value'] = array('title' => '', 'url' => '', 'internal' => '');
-
-		// foreach choices
-
-		foreach($field['value'] as $key => $value)
-		{
-			
-			if($key=='internal' && $field['internal_link'] == 'true'){
-			echo '<td>';
-				echo '<ul class="checkbox_list true_false"><input type="hidden" name="'.$field['name'].'['.$key.']" value="0" />';
-				$selected = ($value == 1) ? 'checked="yes"' : '';
-				echo '<li><label><input type="checkbox" name="'.$field['name'].'['.$key.']" value="1" ' . $selected . ' />';
-				echo '</label></li></ul>';
-
-		echo '</td>';
-
-			}elseif($key=='title'  && $field['website_title'] == 'true'){
-
-echo '<td>';
-				echo '<input type="text" value="' . $value . '" id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '['.$key.']" />';
-
-		echo '</td>';
-
-			}elseif($key=='url'){
-echo '<td>';
-				echo '<input type="text" value="' . $value . '" id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '['.$key.']" />';
-					echo '</td>';
-					};
-
-	
-
-
-		}
-
-		echo  '</tr></tbody>
-			</table>';
-
-
 
 	}
 
 
-
-
-
-
-
-	/*--------------------------------------------------------------------------------------
+	/*
+	*  input_admin_head()
 	*
-	*	admin_head
-	*	- this function is called in the admin_head of the edit screen where your field
-	*	is created. Use this function to create css and javascript to assist your
-	*	create_field() function.
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add css and javascript to assist your create_field() action.
 	*
-	*	@author Elliot Condon
-	*	@since 2.2.0
-	*
-	*-------------------------------------------------------------------------------------*/
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
 
-	function admin_head()
+	function input_admin_head()
 	{
-?>
-		<style type="text/css">
-
-
-		</style>
-		<?php
-	}
-
-
-
-
-
-	/*--------------------------------------------------------------------------------------
-	*
-	*	pre_save_field
-	*	- called just before saving the field to the database.
-	*
-	*	@author Elliot Condon
-	*	@since 2.2.0
-	*
-	*-------------------------------------------------------------------------------------*/
-
-	function pre_save_field($field)
-	{
-		// defaults
-		$field_links = isset($field['website']) ? $field_links = $field['website'] : $field_links = array('title' => '', 'url' => '', 'internal' => '');
-	
-	
-			// vars
-		$new_choices = array();
-
-
-		// key => value
-		foreach($field_links as $choice)
-		{
-			$choice =  ereg_replace("(https?)://", "", $choice);
-			$new_choices[trim($choice)] = trim($choice);
-		}
-
-		// update choices
-		$field['website'] = $new_choices;
-
-		// return updated field
-		return $field;
 
 	}
 
 
-
-
-
-	/*--------------------------------------------------------------------------------------
+	/*
+	*  field_group_admin_enqueue_scripts()
 	*
-	*	get_value_for_api
-	*	- called from your template file when using the API functions (get_field, etc).
-	*	This function is useful if your field needs to format the returned value
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add css + javascript to assist your create_field_options() action.
 	*
-	*	@params
-	*	- $post_id (int) - the post ID which your value is attached to
-	*	- $field (array) - the field object.
-	*
-	*	@author Elliot Condon
-	*	@since 3.0.0
-	*
-	*-------------------------------------------------------------------------------------*/
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
 
-	function get_value_for_api($post_id, $field)
+	function field_group_admin_enqueue_scripts()
 	{
-		// get value
-		$value = $this->get_value($post_id, $field);
-
-		// format value
-		
-			//If an external link
-			if($value['internal'] == 0){ $external = 'target="_blank"';};
-			
-			//If show title
-			if(!empty($value['title'])){ $title = $value['title'];}else{$title = $value['url'];};
-		
-				$is_link = $value['url'];
-
-				$value ='<a href="http://'.$value['url'].'" '.$external.'>'.$title.'</a>';
-		
-		
-		
-
-		// return value
-		if(!empty($is_link) ){
-		    return $value;
-		};
-		
 
 	}
 
+
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add css and javascript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+
+	}
 }
+
+
+// create field
+new Website_field();
+
+
+/*--------------------------------------- fuctions.php ----------------------------------------------------
+
+add_action('acf/register_fields', 'my_register_fields');
+
+function my_register_fields()
+{
+	include_once('fields/my-field.php');
+}
+*/
 
 ?>
