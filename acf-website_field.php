cf5227313ee2b9e5bc32f49951ed142d059e6150
Geet Jacobs <dale@geetdesign.com>
2013-07-04 13:23:01 -0600

    Update acf-website_field.php

--- acf-website_field.php
+++ acf-website_field.php
@@ -21,7 +21,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'raw_url' => 'https://raw.github.com/Jeradin/acf-website-field/Development', // the github raw url of your github repo
         'github_url' => 'https://github.com/Jeradin/acf-website-field', // the github url of your github repo
         'zip_url' => 'https://github.com/Jeradin/acf-website-field/zipball/Development', // the zip url of the github repo
-        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
         'readme' => 'README.md', // which file to use as the readme for the version number
