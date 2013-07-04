72cb54286e5c256fde5304f44a3e5a40bdd9053b
Geet Jacobs <dale@geetdesign.com>
2013-07-04 13:21:38 -0600

    Update acf-website_field.php

--- acf-website_field.php
+++ acf-website_field.php
@@ -18,9 +18,9 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
         'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
         'proper_folder_name' => 'acf-website-field', // this is the name of the folder your plugin lives in
         'api_url' => 'https://github.com/Jeradin/acf-website-field', // the github API url of your github repo
-        'raw_url' => 'https://raw.github.com/Jeradin/acf-website-field/master', // the github raw url of your github repo
+        'raw_url' => 'https://raw.github.com/Jeradin/acf-website-field/Development', // the github raw url of your github repo
         'github_url' => 'https://github.com/Jeradin/acf-website-field', // the github url of your github repo
-        'zip_url' => 'https://github.com/Jeradin/acf-website-field/zipball/master', // the zip url of the github repo
+        'zip_url' => 'https://github.com/Jeradin/acf-website-field/zipball/Development', // the zip url of the github repo
         'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
         'requires' => '3.0', // which version of WordPress does your plugin require?
         'tested' => '3.3', // which version of WordPress is your plugin tested up to?
