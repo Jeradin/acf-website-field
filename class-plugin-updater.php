cc82528d92ae47a106e99bb98e2a1308c34af452
Geet Jacobs <geet@anagr.am>
2013-09-11 09:29:34 -0600

    Fixed issue when field is required, updated updater

--- class-plugin-updater.php
+++ class-plugin-updater.php
@@ -1,78 +1,111 @@
 <?php
-
+/**
+ * GitHub Updater
+ *
+ * @package   GitHubUpdater
+ * @author    Andy Fragen, Codepress
+ * @license   GPL-2.0+
+ * @link      https://github.com/afragen/github-updater
+ */
 
 /**
- * Update a WordPress plugin via GitHub
+ * Update a WordPress plugin from a GitHub repo.
  *
- *
- * @version 1.0
+ * @package GitHubUpdater
+ * @author  Andy Fragen, Codepress
  */
 class GitHub_Plugin_Updater {
 
 	/**
-	 * Stores the config.
+	 * Store details of all GitHub-sourced plugins that are installed.
 	 *
-	 * @since 1.0
-	 * @var type
+	 * @since 1.0.0
+	 *
+	 * @var array
 	 */
 	protected $config;
+
+	/**
+	 * Store details for one GitHub-sourced plugin during the update procedure.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
 	protected $github_plugin;
 
 	/**
 	 * Constructor.
 	 *
-	 * @since 1.0
+	 * @since 1.0.0
+	 *
 	 * @param array $config
 	 */
 	public function __construct() {
-
+		// This MUST come before we get details about the plugins so the headers are correctly retrieved
 		add_filter( 'extra_plugin_headers', array( $this, 'add_headers' ) );
 
+		// Get details of GitHub-sourced plugins
 		$this->config = $this->get_plugin_meta();
-		
+
 		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'update_available' ) );
 		add_filter( 'upgrader_source_selection', array( $this, 'upgrader_source_selection_filter' ), 10, 3 );
-		add_action( 'http_request_args', array( $this, 'no_ssl_http_request_args' ), 10, 2 );
+		add_action( 'http_request_args', array( $this, 'no_ssl_http_request_args' ) );
 	}
 
 	/**
 	 * Add extra header from plugin 'GitHub Plugin URI'
 	 *
+	 * @since 1.0.0
 	 */
 	public function add_headers( $extra_headers ) {
-		$extra_headers = array( 'GitHub Plugin URI', 'GitHub Access Token' );
+		$extra_headers = array( 'GitHub Plugin URI', 'GitHub Access Token', 'GitHub Branch' );
 		return $extra_headers;
 	}
 
+	/**
+	 * Get details of GitHub-sourced plugins from those that are installed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array Indexed array of associative arrays of plugin details.
+	 */
 	protected function get_plugin_meta() {
+		// Ensure get_plugins() function is available.
 		include_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-		$plugins = get_plugins();
-		$i = 0;
+
+		$plugins        = get_plugins();
+		$github_plugins = array();
+		$i              = 0;
 
 		foreach ( $plugins as $plugin => $headers ) {
-			if ( ! empty($headers['GitHub Plugin URI']) ) {
-				$repo = explode( '/', ltrim( parse_url( $headers['GitHub Plugin URI'], PHP_URL_PATH ), '/' ) );
-				$arr[$i]['owner']        = $repo[0];
-				$arr[$i]['repo']         = $repo[1];
-				$arr[$i]['slug']         = $plugin;
-				$arr[$i]['uri']          = $headers['GitHub Plugin URI'];
-				$arr[$i]['access_token'] = $headers['GitHub Access Token'];
-				$i++;
-			}
+			if ( empty( $headers['GitHub Plugin URI'] ) )
+				continue;
+
+			$repo = explode( '/', ltrim( parse_url( $headers['GitHub Plugin URI'], PHP_URL_PATH ), '/' ) );
+			$github_plugins[$i]['owner']        = $repo[0];
+			$github_plugins[$i]['repo']         = $repo[1];
+			$github_plugins[$i]['slug']         = $plugin;
+			$github_plugins[$i]['uri']          = $headers['GitHub Plugin URI'];
+			$github_plugins[$i]['access_token'] = $headers['GitHub Access Token'];
+			$github_plugins[$i]['branch']       = $headers['GitHub Branch'];
+			$i++;
 		}
-		return $arr;
+		return $github_plugins;
 	}
 
 	/**
 	 * Call the GitHub API and return a json decoded body.
 	 *
-	 * @since 1.0
-	 * @param string $url
+	 * @since 1.0.0
+	 *
 	 * @see http://developer.github.com/v3/
+	 *
+	 * @param string $url
+	 *
 	 * @return boolean|object
 	 */
 	protected function api( $url ) {
-
 		$response = wp_remote_get( $this->get_api_url( $url ) );
 
 		if ( is_wp_error( $response ) || wp_remote_retrieve_response_code( $response ) != '200' )
@@ -84,45 +117,57 @@ class GitHub_Plugin_Updater {
 	/**
 	 * Return API url.
 	 *
-	 * @todo Maybe allow a filter to add or modify segments.
-	 * @since 1.0
+	 * @since 1.0.0
+	 *
 	 * @param string $endpoint
+	 *
 	 * @return string
 	 */
 	protected function get_api_url( $endpoint ) {
-
 		$segments = array(
 			'owner'          => $this->github_plugin['owner'],
 			'repo'           => $this->github_plugin['repo'],
-			'archive_format' => 'zipball',
 		);
+
+		/**
+ 		 * Add or filter the available segments that are used to replace placeholders.
+		 *
+		 * @since 1.5.0
+		 *
+		 * @param array $segments List of segments.
+		 */
+		$segments = apply_filters( 'github_updater_api_segments', $segments );
 
 		foreach ( $segments as $segment => $value ) {
 			$endpoint = str_replace( '/:' . $segment, '/' . $value, $endpoint );
 		}
 
 		if ( ! empty( $this->github_plugin['access_token'] ) )
-			$endpoint = add_query_arg( 'access_token', $this->github_plugin['access_token'] );
+			$endpoint = add_query_arg( 'access_token', $this->github_plugin['access_token'], $endpoint );
+
+		// If a branch has been given, only check that for the remote info.
+		// If it's not been given, GitHub will use the Default branch.
+		if ( ! empty( $this->github_plugin['branch'] ) )
+			$endpoint = add_query_arg( 'ref', $this->github_plugin['branch'], $endpoint );
 
 		return 'https://api.github.com' . $endpoint;
 	}
 
 	/**
-	 * Reads the remote plugin file.
+	 * Read the remote plugin file.
 	 *
 	 * Uses a transient to limit the calls to the API.
 	 *
-	 * @since 1.0
+	 * @since 1.0.0
 	 */
 	protected function get_remote_info() {
-
 		$remote = get_site_transient( md5( $this->github_plugin['slug'] ) );
 
 		if ( ! $remote ) {
 			$remote = $this->api( '/repos/:owner/:repo/contents/' . basename( $this->github_plugin['slug'] ) );
 
 			if ( $remote )
-				set_site_transient( md5( $this->github_plugin['slug'] ), $remote, 60 * 60 );
+				set_site_transient( md5( $this->github_plugin['slug'] ), $remote, HOUR_IN_SECONDS );
 		}
 		return $remote;
 	}
@@ -130,11 +175,11 @@ class GitHub_Plugin_Updater {
 	/**
 	 * Retrieves the local version from the file header of the plugin
 	 *
-	 * @since 1.0
-	 * @return string|boolean
+	 * @since 1.0.0
+	 *
+	 * @return string|boolean Version of installed plugin, false if not determined.
 	 */
 	protected function get_local_version() {
-
 		$data = get_plugin_data( WP_PLUGIN_DIR . '/' . $this->github_plugin['slug'] );
 
 		if ( ! empty( $data['Version'] ) )
@@ -144,18 +189,18 @@ class GitHub_Plugin_Updater {
 	}
 
 	/**
-	 * Retrieves the remote version from the file header of the plugin
+	 * Retrieve the remote version from the file header of the plugin
 	 *
-	 * @since 1.0
-	 * @return string|boolean
+	 * @since 1.0.0
+	 *
+	 * @return string|boolean Version of remote plugin, false if not determined.
 	 */
 	protected function get_remote_version() {
-
 		$response = $this->get_remote_info();
 		if ( ! $response )
 			return false;
 
-		preg_match( '#^\s*Version\:\s*(.*)$#im', base64_decode( $response->content ), $matches );
+		preg_match( '/^[ \t\/*#@]*Version\:\s*(.*)$/im', base64_decode( $response->content ), $matches );
 
 		if ( ! empty( $matches[1] ) )
 			return $matches[1];
@@ -164,15 +209,41 @@ class GitHub_Plugin_Updater {
 	}
 
 	/**
-	 * Hooks into pre_set_site_transient_update_plugins to update from GitHub.
+	 * Parse the remote info to find what the default branch is.
 	 *
-	 * @since 1.0
+	 * @since 1.5.0
+	 *
+	 * @return string Default branch name.
+	 */
+	protected function get_default_branch() {
+		// If we've had to call this default branch method, we know that a branch header has not been provided. As such
+		// the remote info was retrieved without a ?ref=... query argument.
+		$response = $this->get_remote_info();
+
+		// If we can't contact GitHub API, then assume a sensible default in case the non-API part of GitHub is working.
+		if ( ! $response )
+			return 'master';
+
+		// Assuming we've got some remote info, parse the 'url' field to get the last bit of the ref query string
+		$components = parse_url( $response->url, PHP_URL_QUERY );
+		parse_str( $components );
+		return $ref;
+	}
+
+	/**
+	 * Hook into pre_set_site_transient_update_plugins to update from GitHub.
+	 *
+	 * The branch to download is hard-coded as the Master branch. Consider using Git-Flow so that Master is always clean.
+	 *
 	 * @todo fill url with value from remote repostory
-	 * @param $transient
-	 * @return $transient If all goes well, an updated one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param object $transient Original transient.
+	 *
+	 * @return $transient If all goes well, an updated transient that may include details of a plugin update.
 	 */
 	public function update_available( $transient ) {
-
 		if ( empty( $transient->checked ) )
 			return $transient;
 
@@ -180,7 +251,10 @@ class GitHub_Plugin_Updater {
 			$this->github_plugin = $plug;
 			$local_version  = $this->get_local_version();
 			$remote_version = $this->get_remote_version();
-			$download_link = trailingslashit( $this->github_plugin['uri'] ) . 'archive/master.zip';
+
+			$branch = $this->github_plugin['branch'] ? $this->github_plugin['branch'] : $this->get_default_branch();
+
+			$download_link = trailingslashit( $this->github_plugin['uri'] ) . 'archive/' . $branch . '.zip';
 
 			if ( $local_version && $remote_version && version_compare( $remote_version, $local_version, '>' ) ) {
 				$plugin = array(
@@ -196,44 +270,72 @@ class GitHub_Plugin_Updater {
 		return $transient;
 	}
 
-	
 	/**
-	 *	Github delivers zip files as <Username>-<TagName>.zip
-	 *	must rename this zip file to the accurate plugin folder
-	 * 
-	 * @since 1.0
-	 * @param string
-	 * @return string 
+	 * Rename the zip folder to be the same as the existing plugin folder.
+	 *
+	 * Github delivers zip files as <Repo>-<Branch>.zip
+	 *
+	 * @since 1.0.0
+	 *
+	 * @global WP_Filesystem $wp_filesystem
+	 *
+	 * @param string $source
+	 * @param string $remote_source Optional.
+	 * @param object $upgrader      Optional.
+	 *
+	 * @return string
 	 */
-	public function upgrader_source_selection_filter( $source, $remote_source=NULL, $upgrader=NULL ) {
+	public function upgrader_source_selection_filter( $source, $remote_source = null, $upgrader = null ) {
 
-		if( isset( $source ) )
+		global $wp_filesystem;
+		$update = array( 'update-selected', 'update-selected-themes', 'upgrade-theme', 'upgrade-plugin' );
+
+		if ( isset( $source ) ) {
 			for ( $i = 0; $i < count( $this->config ); $i++ ) {
-				if( stristr( basename( $source ), $this->config[$i]['repo'] ) )
+				if ( stristr( basename( $source ), $this->config[$i]['repo'] ) )
 					$plugin = $this->config[$i]['repo'];
 			}
+		}
 
-		if( isset( $_GET['action'] ) && stristr( $_GET['action'], 'update-selected' ) )
-			if( isset( $source, $remote_source, $plugin ) && stristr( basename( $source ), $plugin ) ) {
-				$upgrader->skin->feedback( "Trying to customize plugin folder name..." );
-				$corrected_source = trailingslashit( $remote_source ) . trailingslashit( $plugin );
-				if( @rename( $source, $corrected_source ) ) {
-					$upgrader->skin->feedback( "Plugin folder name corrected to: " . $plugin );
-					return $corrected_source;
-				} else {
-					$upgrader->skin->feedback( "Unable to rename downloaded plugin." );
-					return new WP_Error();
-				}
-			}
-		return $source;
+		// If there's no action set, or not one we recognise, abort
+		if ( ! isset( $_GET['action'] ) || ! in_array( $_GET['action'], $update, true ) )
+			return $source;
+
+		// If the values aren't set, or it's not GitHub-sourced, abort
+		if ( ! isset( $source, $remote_source, $plugin ) || false === stristr( basename( $source ), $plugin ) )
+			return $source;
+
+		$corrected_source = trailingslashit( $remote_source ) . trailingslashit( $plugin );
+		$upgrader->skin->feedback(
+			sprintf(
+				__( 'Renaming %s to %s...', 'github-updater' ),
+				'<span class="code">' . basename( $source ) . '</span>',
+				'<span class="code">' . basename( $corrected_source ) . '</span>'
+			)
+		);
+
+		// If we can rename, do so and return the new name
+		if ( $wp_filesystem->move( $source, $corrected_source, true ) ) {
+			$upgrader->skin->feedback( __( 'Rename successful...', 'github-updater' ) );
+			return $corrected_source;
+		}
+
+		// Otherwise, return an error
+		$upgrader->skin->feedback( __( 'Unable to rename downloaded plugin.', 'github-updater' ) );
+		return new WP_Error();
 	}
 
-	/* https://github.com/UCF/Theme-Updater/issues/3 */
-	public function no_ssl_http_request_args( $args, $url ) {
+	/**
+	 * Fixes {@link https://github.com/UCF/Theme-Updater/issues/3}.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array $args Existing HTTP Request arguments.
+	 *
+	 * @return array Amended HTTP Request arguments.
+	 */
+	public function no_ssl_http_request_args( $args ) {
 		$args['sslverify'] = false;
 		return $args;
 	}
-
 }
-
-new GitHub_Plugin_Updater();
--- website_url.php
+++ website_url.php
@@ -1,341 +1,106 @@
+<?php get_header();  ?>
+	<div id="primary" class="row-fluid">
+		<div id="content" role="main" class="span12">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<article class="post">
+						<div class="post-content">
+<?php //echo get_field('test'); ?>
+
+
+							<h1><?php the_title(); ?></h1><?php the_content(); ?>
+		<h4 class="home-header"><span>Grantee Information</span> <a href="http://<?php echo ereg_replace("(https?)://", "", get_field('website')); ?>" target="_blank" class="granteelink"><?php echo ereg_replace("(https?)://", "", get_field('website')); ?></a></h4>
+	<div class="row-fluid">
+				<div class="span3 grantee-img">  <?php if ( has_post_thumbnail() ) { ?>
+							<?php the_post_thumbnail(); ?>
+						<?php } ?></div>
+							<div class="span9"><?php if(get_field('website')):?><strong>Website(s):</strong> <a href="http://<?php echo ereg_replace("(https?)://", "", get_field('website')); ?>" target="_blank"><?php echo ereg_replace("(https?)://", "", get_field('website')); ?></a><br/><?php endif; ?>
+							<?php if(get_field('secondary_link')):?><a href="http://<?php echo ereg_replace("(https?)://", "", get_field('secondary_link')); ?>" target="_blank"><?php echo ereg_replace("(https?)://", "", get_field('secondary_link')); ?></a><br/><?php endif; ?>
+		  <strong>Year(s) Awarded:</strong> <?php
+						   $terms = wp_get_post_terms($post->ID, 'grantee_year');
+						 $count = count($terms);
+						 if ( $count > 0 ){
+						     foreach ( $terms as $term ) {
+						      $theterms[] = $term->name;
+
+						     }
+
+						     echo ''.implode(', ' , $theterms);
+						 }
+
+						  ?>
+						  <br/>
+
 <?php
+				$related_posts = get_posts(array(
+							'post_type' => 'post',
+							'meta_query' => array(
+								array(
+									'key' => 'related_grantee', // name of custom field
+									  'value' => get_the_ID(),
+									//'value' => '"' . get_the_ID() . '"', // matches exaclty "123", not just 123. This prevents a match for "1234"
+									'compare' => 'LIKE'
+								)
+							)
+						));
 
-/*
-*  Website URL
-*
-*  TThis field lets you add website links with titles and a checkbox to make an external link or internal link.
-*
-*  @since	1.1
-*  @date	06/24/2013
-*
-*  @info	https://github.com/Jeradin/acf-website-field
-*/
-
-class Website_field extends acf_Field
-{
-
-	// vars
-	var  $defaults;
-
-	/*
-	*  __construct
-	*
-	*  Set name / label needed for actions / filters
-	*
-	*  @since	3.6
-	*  @date	23/01/13
-	*/
+						?>
+						<?php if( $related_posts ): ?>
+						<br/><strong>Related Nexus Stories:</strong>
+							<div class="related-posts">
+							<?php foreach( $related_posts as $related ): ?>
 
-	function __construct()
-	{
-		// vars
-		$this->name = 'website';
-		$this->label = __('Website');
-		$this->defaults = array(
-			'default_value'              => '',
-			'internal_link'      => 0,
-			'website_title'      => 0,
-			'output_format'      => 0
+									<a href="<?php echo get_permalink( $related->ID ); ?>">
+										<?php echo get_the_title( $related->ID ); ?>
+									</a>
+								<br/>
+							<?php endforeach; ?>
+							</div>
+						<?php endif; ?>
 
-		);
 
 
-		// do not delete!
-    	parent::__construct();
-	}
 
+</div>
 
-	/*
-	*  format_value_for_api()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	*/
 
 
-	function format_value_for_api(  $value, $post_id, $field )
-	{
 
 
-		// validate
-		if( empty($value['url'] ) )
-		{
-			return false;
-		}
+</div>
+<div class="bottom-meta">
+  <div class="share-this">
 
-		// defaults
-		$field = array_merge($this->defaults, $field);
-		extract( $field, EXTR_SKIP ); //Declare each item in $field as its own variable i.e.: $name, $value, $label, $time_format, $date_format and $show_week_number
 
+		   <?php echo get_share_icons(get_permalink(), get_the_title()) ?>
 
-		// format
-		if( $field['output_format'] == 0 )
-		{
+		</div>
+			<div class="newer-older">
+			<div class="older"><?php previous_post_link('%link', '<i class="icon-angle-left icon-2x"></i>') ?></div><!--.older-->
+			<div class="newer"><?php next_post_link('%link', '<i class="icon-angle-right icon-2x"></i>') ?></div><!--.older-->
+		</div><!--.newer-older-->
+</div>
 
-			// format value
 
-			$external = '';
-			//If an external link
-			if($internal_link==1  && !empty($value['internal']) ){ $external = 'target="_blank"';};
+						</div><!-- the-content -->
 
-			// get value
-			$the_url = $this->nicifyUrl( $value['url'] );
+					</article>
 
-			//If show title
-			if($website_title==1 && !empty($value['title'])){ $title = $value['title'];}else{$title = $the_url;};
 
-			$value ='<a href="http://'.$the_url.'" '.$external.'>'.$title.'</a>';
+				<?php endwhile;  ?>
 
 
-			// return link
-			if($the_url!='Invalid URL'){
-				return $value;
-			}else{
-				return $the_url;
-			}
+			<?php else :  ?>
 
+				<article class="post error">
+					<h1 class="404">Nothing has been posted like that yet</h1>
+				</article>
 
-		}
-		else
-		{
+			<?php endif;  ?>
 
-			//return array
-			$value = array(
-				'title' => $value['title'],
-				'url' => $value['url'],
-				'external' => $value['internal']
-			);
-
-			return $value;
-
-
-		}
-
-
-	}
-
-
-	/*
-	*  create_field()
-	*
-	*  Create the HTML interface for your field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field - an array holding all the field's data
-	*/
-
-
-
-
-	function create_field($field)
-	{
-
-
-		$field = array_merge( $this->defaults, $field );
-		extract( $field, EXTR_SKIP ); //Declare each item in $field as its own variable i.e.: $name, $value, $label, $time_format, $date_format and $show_week_number
-
-		// key is needed in the field names to correctly save the data
-		$key 	= $field['name'];
-		$class 	= $field['class'];
-
-		$value 		= ( isset($field['value']) ) ? $field['value'] : false;
-		$link_url 	= ( isset($value['url']) ) ? $value['url'] : '';
-		$link_title = ( isset($value['title']) ) ? $value['title'] : '';
-
-
-		echo  '<table class="widefat "><thead><tr>';
-
-		 if(  $field['website_title']  ) echo '<th class="title"><span>Title</span></th>';
-		echo '<th class="url"><span>URL</span></th>';
-		if ($field['internal_link']==1)echo '<th class="internal" style="width:10%;"><span>Internal Link</span></th>';
-
-		echo '</tr></thead><tbody><tr>';
-
-
-
-	    if(  $field['website_title']  ){
-				echo '<td><input type="text" value="' . $link_title . '" id="' . $key . '" class="' . $class . '" name="'.$key.'[title]" /></td>';
-		} else {
-				echo '<input type="hidden" value="" id="' . $field['name'] . '" class="' . $class . '" name="'.$key.'[title]" />';
-		}
-
-echo '<td><input type="text" value="' . $link_url . '" id="' . $field['name'] . '" class="' . $class . '" name="'.$key.'[url]" /><p class="description">You can exclude http://, the field will add it, if missing.</p></td>';
-
-
-			if($field['internal_link']==1){
-			echo '<td><ul class="checkbox_list true_false"><input type="hidden" name="'.$key.'[internal]" value="0" />';
-				$selected = ($field['value']['internal'] == 1) ? 'checked="yes"' : '';
-				echo '<li><label><input type="checkbox" name="'.$key.'[internal]" value="1" ' . $selected . ' />';
-				echo '</label></li></ul></td>';
-
-			}else{
-				echo '<input type="hidden" name="'.$key.'[internal]" value="0" />';
-			}
-
-
-		echo  '</tr>
-		</tbody>
-			</table>';
-
-
-
-	}
-
-
-	/*
-	*  create_options()
-	*
-	*  Create extra options for your field. This is rendered when editing a field.
-	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
-	*
-	*  @type	action
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$field	- an array holding all the field's data
-	*/
-
-	function create_options($field)
-	{
-
-
-
-		// defaults
-		$field = array_merge($this->defaults, $field);
-
-
-		// key is needed in the field names to correctly save the data
-		$key = $field['name'];
-
-
-?>
-		<tr class="field_option field_option_<?php echo $this->name; ?>">
-			<td class="label">
-				<label><?php _e("Show Title?",'acf'); ?></label>
-				<p class="description">If "Yes" than the user will have a field to enter a address title</p>
-			</td>
-			<td>
-				<?php
-
-		do_action('acf/create_field', array(
-				'type' => 'radio',
-				'name' => 'fields['.$key.'][website_title]',
-				'value' => $field['website_title'],
-				'layout' => 'horizontal',
-				'choices' => array(
-					1 => __('Yes','acf'),
-					0 => __('No','acf')
-				)
-			));
-
-
-?>
-			</td>
-
-		</tr>
-
-		<tr class="field_option field_option_<?php echo $this->name; ?>">
-			<td class="label">
-				<label><?php _e("Open in Current Window?",'acf'); ?></label>
-				<p class="description">If "Yes" than the user can check a box to have the link be Internal</p>
-			</td>
-			<td>
-				<?php
-		do_action('acf/create_field', array(
-				'type' => 'radio',
-				'name' => 'fields['.$key.'][internal_link]',
-				'value' => $field['internal_link'],
-				'layout' => 'horizontal',
-				'choices' => array(
-					1 => __('Yes','acf'),
-					0 => __('No','acf')
-				)
-			));
-?>
-			</td>
-
-		</tr>
-				<tr class="field_option field_option_<?php echo $this->name; ?>">
-			<td class="label">
-				<label><?php _e("Return array instead of Link",'acf'); ?></label>
-				<p class="description">If "Yes" than the get_field will return an array</p>
-			</td>
-			<td>
-				<?php
-		do_action('acf/create_field', array(
-				'type' => 'radio',
-				'name' => 'fields['.$key.'][output_format]',
-				'value' => $field['output_format'],
-				'layout' => 'horizontal',
-				'choices' => array(
-					1 => __('Yes','acf'),
-					0 => __('No','acf')
-				)
-			));
-?>
-			</td>
-
-		</tr>
-
-		<?php
-	}
-
-
-
-
-		/** Custom functions to validate URL**/
-
-		function nicifyUrl($url) {
-			$fullurl = $url;
-		    $url = urldecode(strtolower($url));
-		    $url = $this->stripit($url);
-		    // clean up url path
-		    $url = explode("/",$url);
-		    $arrUrl = parse_url($url[0]);
-		    $urlRet = $arrUrl['path'];
-		    // valid?
-		    if($this->validateTheUrl($urlRet)){
-		        return $fullurl;
-		    }
-		    else{
-		        return "Invalid URL";
-		    }
-		}
-		function stripit($url){
-		   $url = trim($url);
-		   $url = preg_replace("/^(http:\/\/)*(www.)*/is", "", $url);
-		   $url = preg_replace("/\/.*$/is" , "" ,$url);
-		   return $url;
-		}
-		function validateTheUrl($url){
-		    if (!preg_match("#[a-z0-9-_.]+\.[a-z]{2,4}#i",$url)) {
-		        return false;
-		    }
-		    else if(strpos($url,"@") !== false){
-		        return false;
-		    }
-		    else{
-		        return true;
-		    }
-		}
-
-
-}
-
-
-// create field
-new Website_field();
-
-
-?>
+		</div><!-- #content .site-content -->
+	</div><!-- #primary .content-area -->
+<?php get_footer(); ?>
