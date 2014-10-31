2b9bef133d820289cbac91d4b1575ef9f84cee94
Geet Jacobs <geetjacobs@gmail.com>
2014-10-31 15:05:16 -0600

    github updater, version number
    Make sure github updater is in header, changed version number

--- acf-website_field.php
+++ acf-website_field.php
@@ -2,13 +2,14 @@
 /*
 Plugin Name: Advanced Custom Fields: Website Field
 Plugin URI: https://github.com/Jeradin/acf-website-field
-GitHub Plugin URI: https://github.com/Jeradin/acf-website-field
 Description: Website Title field for Advanced Custom Fields
-Version: 1.5.9.5
+Version: 2.0
 Author: Geet Jacobs
 Author URI: http://anagr.am
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/Jeradin/acf-website-field
+GitHub Branch:     master
 */
 
 
